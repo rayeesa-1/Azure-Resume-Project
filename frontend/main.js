ee062265b0348ccdf0fc3d6b1d579ea969e2fb74
szs <szs@gmail.com>
2024-09-14 20:02:39 +0530

    Created azure function as api

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,11 @@
-window.addEventListener('DOMContentLoaded',(event)=>{getVisitCount();})
+window.addEventListener('DOMContentLoaded', (event)=>{
+    getVisitCount();})
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () =>{
     let count = 30
-    fetch(functionApi).then(resonse =>{
+    fetch(functionApi).then(response =>{
         return response.json()
     }).then(response =>{
         console.log("website called function API.");
